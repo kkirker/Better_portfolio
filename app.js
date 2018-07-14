92ad8ae2ffbd015e5cb2717811ee7306a5e325f6
Kamon <Kamon.kirker@gmail.com>
2018-07-14 00:19:10 -0700

    scroll working

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+window.onbeforeunload = function(){ window.scrollTo(0,0); }
+
 $(window).load(function () {
   $('#gallery').jqcarousel();
   });
+
+  $("#about-btn").on('click', function() {
+    console.log('button clicked about');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#about-section").offset().top
+    }, 2000);
+});
+
+$("#form-btn").on('click', function() {
+  console.log('button clicked contact');
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#form-section").offset().top
+  }, 2000);
+});
