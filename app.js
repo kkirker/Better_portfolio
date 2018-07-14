f97615c5aff173064be3e247b9eead4b9bf11012
Kamon <Kamon.kirker@gmail.com>
2018-07-14 00:39:52 -0700

    carousels linked to projects

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 window.onbeforeunload = function(){ window.scrollTo(0,0); }
 
+
 $(window).load(function () {
   $('#gallery').jqcarousel();
-  });
+  let count = 0;
+  console.log(count);
 
   $("#about-btn").on('click', function() {
     console.log('button clicked about');
@@ -17,3 +19,51 @@ $("#form-btn").on('click', function() {
       scrollTop: $("#form-section").offset().top
   }, 2000);
 });
+
+$('#crystals').on('click', function() {
+  console.log('crystal clicked');
+  count++;
+  if (count === 2) {
+    window.location = "https://kkirker.github.io/unit-4-game/";
+  }
+});
+
+$('#trivia').on('click', function() {
+  console.log('trivia clicked');
+  count++;
+  if (count === 2) {
+    window.location = "https://kkirker.github.io/TriviaGame/";
+  }
+});
+
+$('#train').on('click', function() {
+  console.log('train clicked');
+  count++;
+  if (count === 2) {
+    window.location = "https://kkirker.github.io/train-game/";
+  }
+});
+
+$('#music').on('click', function() {
+  console.log('music clicked');
+  count++;
+  if (count === 2) {
+    window.location = "https://christiehennes.github.io./Project1/";
+  }
+});
+
+$('#poke').on('click', function() {
+  console.log('poke clicked');
+  count++;
+  if (count === 2) {
+    window.location = "https://kkirker.github.io/Giphy-HW/";
+  }
+});
+
+
+
+
+
+});
+
+
